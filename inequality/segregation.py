2933265d84d81fcb22370f6d2d0791d055404620
renanxcortes <renanxcortes@gmail.com>
2018-10-01 15:12:57 -0700

    adding new functions to the .py archive

--- inequality/segregation.py
+++ inequality/segregation.py
@@ -15,7 +15,7 @@ from sklearn.metrics.pairwise import euclidean_distances
 from scipy.ndimage.interpolation import shift
 
 
-# Function that calculates a specific an overall segregation index
+# Function that calculates several segregation measures and an overall segregation index
 def calculate_segregation(data, group_pop_var, total_pop_var):
     '''
     data: a geopandas DataFrame that contains a geometry column
@@ -27,14 +27,14 @@ def calculate_segregation(data, group_pop_var, total_pop_var):
     data = data.rename(columns={group_pop_var: 'group_pop_var', total_pop_var: 'total_pop_var'})
     T = data.total_pop_var.sum()
     P = data.group_pop_var.sum() / T
-    data['ti'] = data.total_pop_var
-    data['pi'] = data.group_pop_var / data.total_pop_var
+    data = data.assign(xi = data.group_pop_var,
+                       yi = data.total_pop_var - data.group_pop_var,
+                       ti = data.total_pop_var,
+                       pi = data.group_pop_var / data.total_pop_var)
     D = (((data.total_pop_var * abs(data.pi - P)))/ (2 * T * P * (1 - P))).sum()
     
     # Isolation
-    data['xi'] = data.group_pop_var
     X = data['xi'].sum()
-    data['yi'] = data.total_pop_var - data.group_pop_var
     xPx = ((data.xi / X) * (data.xi / data.ti)).sum()
     xPy = ((data.xi / X) * (data.yi / data.ti)).sum()
     
@@ -42,8 +42,8 @@ def calculate_segregation(data, group_pop_var, total_pop_var):
     
     # Clustering
     Y = data.yi.sum()
-    data['c_lons'] = data.centroid.map(lambda p: p.x)
-    data['c_lats'] = data.centroid.map(lambda p: p.y)
+    data = data.assign(c_lons = data.centroid.map(lambda p: p.x),
+                       c_lats = data.centroid.map(lambda p: p.y))
     dist = euclidean_distances(data[['c_lons','c_lats']])
     np.fill_diagonal(dist, val = (0.6*data.area)**(1/2))
     c = np.exp(-dist)
@@ -59,7 +59,7 @@ def calculate_segregation(data, group_pop_var, total_pop_var):
     DEL = 1/2 * abs(data.xi / X - data.area / A).sum()
     
     df_mp_sort_area = data
-    df_mp_sort_area['area'] = df_mp_sort_area.area
+    df_mp_sort_area = df_mp_sort_area.assign(area = df_mp_sort_area.area)
     df_mp_sort_area_asc = df_mp_sort_area.sort_values('area')
     n1 = np.where(((np.cumsum(df_mp_sort_area_asc.ti) / T) < X/T) == False)[0][0]
     
@@ -76,14 +76,14 @@ def calculate_segregation(data, group_pop_var, total_pop_var):
 
     
     # Centralization
-    data['center_lon']  = data.c_lons.mean()
-    data['center_lat']  = data.c_lats.mean()
+    data = data.assign(center_lon = data.c_lons.mean(),
+                       center_lat = data.c_lats.mean())
     data['center_dist'] = np.sqrt((data.c_lons - data.center_lon)**2 + (data.c_lats - data.center_lat)**2)
     data_sort_cent = data.sort_values('center_dist')
     
-    data_sort_cent['Xi'] = np.cumsum(data_sort_cent.xi) / X
-    data_sort_cent['Yi'] = np.cumsum(data_sort_cent.yi) / Y
-    data_sort_cent['Ai'] = np.cumsum(data_sort_cent.area) / A
+    data_sort_cent = data_sort_cent.assign(Xi = np.cumsum(data_sort_cent.xi) / X,
+                                           Yi = np.cumsum(data_sort_cent.yi) / Y,
+                                           Ai = np.cumsum(data_sort_cent.area) / A)
     
     ACE = (shift(data_sort_cent.Xi, 1, cval=np.NaN) * data_sort_cent.Ai).sum() - \
           (data_sort_cent.Xi * shift(data_sort_cent.Ai, 1, cval=np.NaN)).sum()
@@ -94,15 +94,116 @@ def calculate_segregation(data, group_pop_var, total_pop_var):
     # Aggregating
     SM = np.mean([D, CISO, RCL, RCO, RCE])
     
-    return {'Uneveness': D, 
+    return {'Uneveness (D)': D, 
             'Isolation (xPx)': xPx, 
             'Isolation (xPy)': xPy, 
-            'Centralized Isolation': CISO,
-            'Spatial Proximity (clustering)': SP, 
-            'Relative Clustering': RCL, 
-            'Delta (concentration)': DEL,
-            'Relative Concentration': RCO,
-            'Absolute Centralization': ACE,
-            'Relative Centralization': RCE,
-            'Overall Segregation Measure': SM
+            'Centralized Isolation (CISO)': CISO,
+            'Spatial Proximity (clustering) (SP)': SP, 
+            'Relative Clustering (RCL)': RCL, 
+            'Delta (concentration) (DEL)': DEL,
+            'Relative Concentration (RCO)': RCO,
+            'Absolute Centralization (ACE)': ACE,
+            'Relative Centralization (RCE)': RCE,
+            'Overall Segregation Measure (SM)': SM
            }
+		   
+		   
+		   
+		   
+# Function that calculates only an overall segregation measure		   
+def calculate_only_segregation(data, group_pop_var, total_pop_var):
+    '''
+    data: a geopandas DataFrame that contains a geometry column
+    group_pop_var: the name of variable that contains the population size of the group of interest
+    total_pop_var: the name of variable that contains the total population of the unit
+    '''
+    
+    # Uneveness
+    data = data.rename(columns={group_pop_var: 'group_pop_var', total_pop_var: 'total_pop_var'})
+    data = data[['geometry', 'group_pop_var', 'total_pop_var']]
+    T = data.total_pop_var.sum()
+    P = data.group_pop_var.sum() / T
+    data = data.assign(xi = data.group_pop_var,
+                       yi = data.total_pop_var - data.group_pop_var,
+                       ti = data.total_pop_var,
+                       pi = data.group_pop_var / data.total_pop_var)
+    D = (((data.total_pop_var * abs(data.pi - P)))/ (2 * T * P * (1 - P))).sum()
+    
+    # Isolation
+    X = data['xi'].sum()
+    xPx = ((data.xi / X) * (data.xi / data.ti)).sum()
+    
+    CISO = xPx - X/T
+    
+    # Clustering
+    Y = data.yi.sum()
+    data = data.assign(c_lons = data.centroid.map(lambda p: p.x),
+                       c_lats = data.centroid.map(lambda p: p.y))
+    dist = euclidean_distances(data[['c_lons','c_lats']])
+    np.fill_diagonal(dist, val = (0.6*data.area)**(1/2))
+    c = np.exp(-dist)
+    Pxx = ((np.array(data.xi) * c).T * np.array(data.xi)).sum() / X**2
+    Pyy = ((np.array(data.yi) * c).T * np.array(data.yi)).sum() / Y**2
+
+    RCL = Pxx / Pyy - 1
+
+    # Concentration   
+    df_mp_sort_area = data
+    df_mp_sort_area = df_mp_sort_area.assign(area = df_mp_sort_area.area)
+    df_mp_sort_area_asc = df_mp_sort_area.sort_values('area')
+    n1 = np.where(((np.cumsum(df_mp_sort_area_asc.ti) / T) < X/T) == False)[0][0]
+    
+    df_mp_sort_area_des = df_mp_sort_area.sort_values('area', ascending=False)
+    n2 = np.where(((np.cumsum(df_mp_sort_area_des.ti) / T) < X/T) == False)[0][0]
+    
+    n = df_mp_sort_area_asc.shape[0]
+    T1 =  df_mp_sort_area_asc.ti[0:(n1+1)].sum()
+    T2 =  df_mp_sort_area_asc.ti[n2:n].sum()
+
+    RCO = ((((df_mp_sort_area_asc.xi*df_mp_sort_area_asc.area/X).sum()) / ((df_mp_sort_area_asc.yi*df_mp_sort_area_asc.area/Y).sum())) - 1) / \
+          ((((df_mp_sort_area_asc.ti*df_mp_sort_area_asc.area)[0:(n1+1)].sum() / T1) / ((df_mp_sort_area_asc.ti*df_mp_sort_area_asc.area)[n2:n].sum() / T2)) - 1)
+
+    
+    # Centralization
+    data = data.assign(center_lon = data.c_lons.mean(),
+                       center_lat = data.c_lats.mean())
+    data['center_dist'] = np.sqrt((data.c_lons - data.center_lon)**2 + (data.c_lats - data.center_lat)**2)
+    data_sort_cent = data.sort_values('center_dist')
+    
+    data_sort_cent = data_sort_cent.assign(Xi = np.cumsum(data_sort_cent.xi) / X,
+                                           Yi = np.cumsum(data_sort_cent.yi) / Y)
+    
+    RCE = (shift(data_sort_cent.Xi, 1, cval=np.NaN) * data_sort_cent.Yi).sum() - \
+          (data_sort_cent.Xi * shift(data_sort_cent.Yi, 1, cval=np.NaN)).sum()
+    
+    # Aggregating
+    SM = np.mean([D, CISO, RCL, RCO, RCE])
+    
+    return SM
+	
+	
+	
+	
+# Function that calculates the overall segregation measure under the null hyphotesis multiple times
+def infer_segregation(data, group_pop_var, total_pop_var, permutations = 1000):
+    '''
+    data: a geopandas DataFrame that contains a geometry column
+    group_pop_var: the name of variable that contains the population size of the group of interest
+    total_pop_var: the name of variable that contains the total population of the unit
+    permutations: number of permutation to compute inference on pseudo p-values
+    '''
+    
+    data = data.rename(columns={group_pop_var: 'group_pop_var', total_pop_var: 'total_pop_var'})
+    p_null = data.group_pop_var.sum() / data.total_pop_var.sum()
+    
+    result_t = []
+    for i in range(permutations):
+        freq_sim = np.random.binomial(n = np.array([data.total_pop_var.tolist()]), 
+                                      p = np.array([[p_null]*data.shape[0]]), 
+                                      size = (1, data.shape[0])).tolist()[0]
+        data['group_pop_var'] = freq_sim
+        val = calculate_only_segregation(data, 'group_pop_var', 'total_pop_var')
+        result_t.append(val)
+    
+    return result_t
+    
