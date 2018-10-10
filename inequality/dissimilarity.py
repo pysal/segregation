28d451d1b02c5370fbba65b0f94b6eac0cf2477a
renanxcortes <renanxcortes@gmail.com>
2018-10-10 14:47:50 -0700

    including classic dissimilarity function

---
+++ inequality/dissimilarity.py
@@ -0,0 +1,112 @@
+"""
+Dissimilarity based Segregation Metrics
+"""
+
+__author__ = "Renan X. Cortes <renanc@ucr.edu> and Sergio J. Rey <sergio.rey@ucr.edu>"
+
+import numpy as np
+import pandas as pd
+
+__all__ = ['Dissim']
+
+
+def _dissim(data, group_pop_var, total_pop_var):
+    """
+    Calculation of Dissimilarity index
+
+    Parameters
+    ----------
+
+    data          : a pandas DataFrame
+    
+    group_pop_var : string
+                    The name of variable in data that contains the population size of the group of interest
+                    
+    total_pop_var : string
+                    The name of variable in data that contains the total population of the unit
+
+    Attributes
+    ----------
+
+    d : float
+        Dissimilarity Index
+
+    Notes
+    -----
+    Based on Massey, Douglas S., and Nancy A. Denton. "The dimensions of residential segregation." Social forces 67.2 (1988): 281-315.
+
+    """
+    if ((type(group_pop_var) is not str) or (type(total_pop_var) is not str)):
+        TypeError('group_pop_var and total_pop_var must be strings')
+    
+    if ((group_pop_var not in data.columns) or (total_pop_var not in data.columns)):    
+        ValueError('group_pop_var and total_pop_var must be variables of data')
+
+    try:
+        data = data.rename(columns={group_pop_var: 'group_pop_var', total_pop_var: 'total_pop_var'})
+    except AttributeError:
+        print('data must be a pandas DataFrame')
+    
+    T = data.total_pop_var.sum()
+    P = data.group_pop_var.sum() / T
+    
+    # If a unit has zero population, the group of interest frequency is zero
+    data = data.assign(pi = np.where(data.total_pop_var == 0, 0, data.group_pop_var/data.total_pop_var))
+    
+    D = (((data.total_pop_var * abs(data.pi - P)))/ (2 * T * P * (1 - P))).sum()
+    return D
+
+
+class Dissim:
+    """
+    Classic Dissimilarity Index
+
+    Parameters
+    ----------
+
+    data          : a pandas DataFrame
+    
+    group_pop_var : string
+                    The name of variable in data that contains the population size of the group of interest
+                    
+    total_pop_var : string
+                    The name of variable in data that contains the total population of the unit
+
+    Attributes
+    ----------
+
+    d : float
+        Dissimilarity Index
+        
+    Examples
+    --------
+    In this example, we will calculate the degree of dissimilatiry (D) for the Riverside County using the census tract data of 2010.
+    
+    Firstly, we need to read the data:
+    
+    >>> url = 'https://raw.githubusercontent.com/renanxcortes/inequality-segregation-supplementary-files/master/data/std_2010_fullcount.csv'
+    >>> census_2010 = pd.read_csv(url, encoding = "ISO-8859-1", sep = ",")
+    
+    Then, we filter only for the desired county (in this case, Riverside County):
+    
+    >>> df = census_2010.loc[census_2010.county == "Riverside County"][['pop10','nhblk10']]
+    
+    The estimated value is estimated below.
+    
+    >>> dissim_index = Dissim(df, 'nhblk10', 'pop10')
+    >>> dissim_index.d
+    0.31565682496226544
+    
+    The interpretation of this value is that 31.57% of the non-hispanic black population would have to move to reach eveness in the Riverside County.
+        
+    Notes
+    -----
+    Based on Massey, Douglas S., and Nancy A. Denton. "The dimensions of residential segregation." Social forces 67.2 (1988): 281-315.
+
+    """
+
+    def __init__(self, data, group_pop_var, total_pop_var):
+
+        self.d = _dissim(data, group_pop_var, total_pop_var)
+
+
