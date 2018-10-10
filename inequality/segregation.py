85a6a5e379c3c3557ca8c2b12c076aa89a8ad0b7
renanxcortes <renanxcortes@gmail.com>
2018-10-09 17:33:22 -0700

    updates inference function and adds local centralization in the .py file

--- inequality/segregation.py
+++ inequality/segregation.py
@@ -30,7 +30,7 @@ def calculate_segregation(data, group_pop_var, total_pop_var):
     data = data.assign(xi = data.group_pop_var,
                        yi = data.total_pop_var - data.group_pop_var,
                        ti = data.total_pop_var,
-                       pi = data.group_pop_var / data.total_pop_var)
+                       pi = np.where(data.total_pop_var == 0, 0, data.group_pop_var/data.total_pop_var))
     D = (((data.total_pop_var * abs(data.pi - P)))/ (2 * T * P * (1 - P))).sum()
     
     
@@ -127,82 +127,7 @@ def calculate_segregation(data, group_pop_var, total_pop_var):
             
             'Overall Segregation Measure (SM)': SM
            }
-		   
-		   
-		   
-		   
-# Function that calculates only an overall segregation measure		   
-def calculate_only_segregation(data, group_pop_var, total_pop_var):
-    '''
-    data: a geopandas DataFrame that contains a geometry column
-    group_pop_var: the name of variable that contains the population size of the group of interest
-    total_pop_var: the name of variable that contains the total population of the unit
-    '''
-    
-    # Uneveness
-    data = data.rename(columns={group_pop_var: 'group_pop_var', total_pop_var: 'total_pop_var'})
-    data = data[['geometry', 'group_pop_var', 'total_pop_var']]
-    T = data.total_pop_var.sum()
-    P = data.group_pop_var.sum() / T
-    data = data.assign(xi = data.group_pop_var,
-                       yi = data.total_pop_var - data.group_pop_var,
-                       ti = data.total_pop_var,
-                       pi = data.group_pop_var / data.total_pop_var)
-    D = (((data.total_pop_var * abs(data.pi - P)))/ (2 * T * P * (1 - P))).sum()
-    
-    # Isolation
-    X = data['xi'].sum()
-    xPx = ((data.xi / X) * (data.xi / data.ti)).sum()
-    
-    CISO = xPx - X/T
-    
-    # Clustering
-    Y = data.yi.sum()
-    data = data.assign(c_lons = data.centroid.map(lambda p: p.x),
-                       c_lats = data.centroid.map(lambda p: p.y))
-    dist = euclidean_distances(data[['c_lons','c_lats']])
-    np.fill_diagonal(dist, val = (0.6*data.area)**(1/2))
-    c = np.exp(-dist)
-    Pxx = ((np.array(data.xi) * c).T * np.array(data.xi)).sum() / X**2
-    Pyy = ((np.array(data.yi) * c).T * np.array(data.yi)).sum() / Y**2
 
-    RCL = Pxx / Pyy - 1
-
-    # Concentration   
-    df_mp_sort_area = data
-    df_mp_sort_area = df_mp_sort_area.assign(area = df_mp_sort_area.area)
-    df_mp_sort_area_asc = df_mp_sort_area.sort_values('area')
-    n1 = np.where(((np.cumsum(df_mp_sort_area_asc.ti) / T) < X/T) == False)[0][0]
-    
-    df_mp_sort_area_des = df_mp_sort_area.sort_values('area', ascending=False)
-    n2 = np.where(((np.cumsum(df_mp_sort_area_des.ti) / T) < X/T) == False)[0][0]
-    
-    n = df_mp_sort_area_asc.shape[0]
-    T1 =  df_mp_sort_area_asc.ti[0:(n1+1)].sum()
-    T2 =  df_mp_sort_area_asc.ti[n2:n].sum()
-
-    RCO = ((((df_mp_sort_area_asc.xi*df_mp_sort_area_asc.area/X).sum()) / ((df_mp_sort_area_asc.yi*df_mp_sort_area_asc.area/Y).sum())) - 1) / \
-          ((((df_mp_sort_area_asc.ti*df_mp_sort_area_asc.area)[0:(n1+1)].sum() / T1) / ((df_mp_sort_area_asc.ti*df_mp_sort_area_asc.area)[n2:n].sum() / T2)) - 1)
-
-    
-    # Centralization
-    data = data.assign(center_lon = data.c_lons.mean(),
-                       center_lat = data.c_lats.mean())
-    data['center_dist'] = np.sqrt((data.c_lons - data.center_lon)**2 + (data.c_lats - data.center_lat)**2)
-    data_sort_cent = data.sort_values('center_dist')
-    
-    data_sort_cent = data_sort_cent.assign(Xi = np.cumsum(data_sort_cent.xi) / X,
-                                           Yi = np.cumsum(data_sort_cent.yi) / Y)
-    
-    RCE = (shift(data_sort_cent.Xi, 1, cval=np.NaN) * data_sort_cent.Yi).sum() - \
-          (data_sort_cent.Xi * shift(data_sort_cent.Yi, 1, cval=np.NaN)).sum()
-    
-    # Aggregating
-    SM = np.mean([D, CISO, RCL, RCO, RCE])
-    
-    return SM
-	
-	
 	
 	
 # Function that calculates the overall segregation measure under the null hyphotesis multiple times
@@ -225,53 +150,55 @@ def infer_segregation(data,
     data = data.rename(columns={group_pop_var: 'group_pop_var', total_pop_var: 'total_pop_var'})
     p_null = data.group_pop_var.sum() / data.total_pop_var.sum()
     
-    Ds    = []
-    Gs    = []
-    Hs    = []
+    Ds    = np.empty(iterations)
+    Gs    = np.empty(iterations)
+    Hs    = np.empty(iterations)
     
-    xPxs  = []
-    xPys  = []
-    CISOs = []
-    RIs   = []
+    xPxs  = np.empty(iterations)
+    xPys  = np.empty(iterations)
+    CISOs = np.empty(iterations)
+    RIs   = np.empty(iterations)
     
-    SPs   = []
-    RCLs  = []
+    SPs   = np.empty(iterations)
+    RCLs  = np.empty(iterations)
     
-    DELs  = []
-    RCOs  = []
+    DELs  = np.empty(iterations)
+    RCOs  = np.empty(iterations)
     
-    ACEs  = []
-    RCEs  = []
+    ACEs  = np.empty(iterations)
+    RCEs  = np.empty(iterations)
     
-    SMs   = []
+    SMs   = np.empty(iterations)
+    
+    local_RCEs   = np.empty(data.shape[0])
     
     if (null_approach == "eveness"):
-        for i in range(iterations):
+        for i in np.array(range(iterations)):
             
             freq_sim = np.random.binomial(n = np.array([data.total_pop_var.tolist()]), 
                                           p = np.array([[p_null]*data.shape[0]]), 
                                           size = (1, data.shape[0])).tolist()[0]
             data = data.assign(group_pop_var = freq_sim)
             test = calculate_segregation(data, 'group_pop_var', 'total_pop_var')
-            Ds.append(list(test.values())[0])
-            Gs.append(list(test.values())[1])
-            Hs.append(list(test.values())[2])
+            Ds[i] = list(test.values())[0]
+            Gs[i] = list(test.values())[1]
+            Hs[i] = list(test.values())[2]
 
-            xPxs.append(list(test.values())[3])
-            xPys.append(list(test.values())[4])
-            CISOs.append(list(test.values())[5])
-            RIs.append(list(test.values())[6])
+            xPxs[i] = list(test.values())[3]
+            xPys[i] = list(test.values())[4]
+            CISOs[i] = list(test.values())[5]
+            RIs[i] = list(test.values())[6]
 
-            SPs.append(list(test.values())[7])
-            RCLs.append(list(test.values())[8])
+            SPs[i] = list(test.values())[7]
+            RCLs[i] = list(test.values())[8]
 
-            DELs.append(list(test.values())[9])
-            RCOs.append(list(test.values())[10])
+            DELs[i] = list(test.values())[9]
+            RCOs[i] = list(test.values())[10]
 
-            ACEs.append(list(test.values())[11])
-            RCEs.append(list(test.values())[12])
+            ACEs[i] = list(test.values())[11]
+            RCEs[i] = list(test.values())[12]
 
-            SMs.append(list(test.values())[13])
+            SMs[i] = list(test.values())[13]
             
 
     if (null_approach == "permutation"):
@@ -279,25 +206,25 @@ def infer_segregation(data,
 
             data = data.assign(geometry = data.geometry[list(np.random.choice(data.shape[0], data.shape[0], replace = False))].reset_index()['geometry'])
             test = calculate_segregation(data, 'group_pop_var', 'total_pop_var')
-            Ds.append(list(test.values())[0])
-            Gs.append(list(test.values())[1])
-            Hs.append(list(test.values())[2])
+            Ds[i] = list(test.values())[0]
+            Gs[i] = list(test.values())[1]
+            Hs[i] = list(test.values())[2]
 
-            xPxs.append(list(test.values())[3])
-            xPys.append(list(test.values())[4])
-            CISOs.append(list(test.values())[5])
-            RIs.append(list(test.values())[6])
+            xPxs[i] = list(test.values())[3]
+            xPys[i] = list(test.values())[4]
+            CISOs[i] = list(test.values())[5]
+            RIs[i] = list(test.values())[6]
 
-            SPs.append(list(test.values())[7])
-            RCLs.append(list(test.values())[8])
+            SPs[i] = list(test.values())[7]
+            RCLs[i] = list(test.values())[8]
 
-            DELs.append(list(test.values())[9])
-            RCOs.append(list(test.values())[10])
+            DELs[i] = list(test.values())[9]
+            RCOs[i] = list(test.values())[10]
 
-            ACEs.append(list(test.values())[11])
-            RCEs.append(list(test.values())[12])
+            ACEs[i] = list(test.values())[11]
+            RCEs[i] = list(test.values())[12]
 
-            SMs.append(list(test.values())[13])
+            SMs[i] = list(test.values())[13]
             
     if (null_approach == "even_permutation"):
         for i in range(iterations):
@@ -308,26 +235,76 @@ def infer_segregation(data,
             data = data.assign(group_pop_var = freq_sim,
                                geometry = data.geometry[list(np.random.choice(data.shape[0], data.shape[0], replace = False))].reset_index()['geometry'])
             test = calculate_segregation(data, 'group_pop_var', 'total_pop_var')
-            Ds.append(list(test.values())[0])
-            Gs.append(list(test.values())[1])
-            Hs.append(list(test.values())[2])
+            Ds[i] = list(test.values())[0]
+            Gs[i] = list(test.values())[1]
+            Hs[i] = list(test.values())[2]
 
-            xPxs.append(list(test.values())[3])
-            xPys.append(list(test.values())[4])
-            CISOs.append(list(test.values())[5])
-            RIs.append(list(test.values())[6])
+            xPxs[i] = list(test.values())[3]
+            xPys[i] = list(test.values())[4]
+            CISOs[i] = list(test.values())[5]
+            RIs[i] = list(test.values())[6]
 
-            SPs.append(list(test.values())[7])
-            RCLs.append(list(test.values())[8])
+            SPs[i] = list(test.values())[7]
+            RCLs[i] = list(test.values())[8]
 
-            DELs.append(list(test.values())[9])
-            RCOs.append(list(test.values())[10])
+            DELs[i] = list(test.values())[9]
+            RCOs[i] = list(test.values())[10]
 
-            ACEs.append(list(test.values())[11])
-            RCEs.append(list(test.values())[12])
+            ACEs[i] = list(test.values())[11]
+            RCEs[i] = list(test.values())[12]
 
-            SMs.append(list(test.values())[13])
+            SMs[i] = list(test.values())[13]
     
     
     return Ds, Gs, Hs, xPxs, xPys, CISOs, RIs, SPs, RCLs, DELs, RCOs, ACEs, RCEs, SMs
+	
+
+	
+# Function that calculates the local centralization for every unit i and spatial extent k (from the k nearest neighbors)
+def local_centralization(data, group_pop_var, total_pop_var, k_neigh):
+    '''
+    data: a geopandas DataFrame that contains a geometry column
+    group_pop_var: the name of variable that contains the population size of the group of interest
+    total_pop_var: the name of variable that contains the total population of the unit
+    k: number of assumed neighbors for local context
+    '''
+    data = data.rename(columns={group_pop_var: 'group_pop_var', total_pop_var: 'total_pop_var'})
+    data = data.assign(xi = data.loc[:,'group_pop_var'],
+                       yi = data.loc[:,'total_pop_var'] - data.loc[:,'group_pop_var'],
+        
+                       c_lons = data.centroid.map(lambda p: p.x),
+                       c_lats = data.centroid.map(lambda p: p.y))
+    
+    points = list(zip(data.c_lons, data.c_lats))
+    kd = ps.cg.kdtree.KDTree(np.array(points))
+    wnnk = ps.weights.KNN(kd, k = k_neigh)
+    
+    local_RCEs   = np.empty(data.shape[0])
+    
+    for i in np.array(range(data.shape[0])):
+    
+        x = list(wnnk.neighbors.values())[i]
+        x.append(list(wnnk.neighbors.keys())[i])
+
+        local_data = data.iloc[x,:]
+
+        local_data = local_data.assign(center_lon = local_data.c_lons.iloc[local_data.shape[0]-1], 
+                                       center_lat = local_data.c_lats.iloc[local_data.shape[0]-1]) 
+
+        local_data['center_dist'] = np.sqrt((local_data.c_lons - local_data.center_lon)**2 + (local_data.c_lats - local_data.center_lat)**2)
+        local_data_sort_cent = local_data.sort_values('center_dist')
+
+        X = local_data_sort_cent.xi.sum()
+        Y = local_data_sort_cent.yi.sum()
+
+        local_data_sort_cent['Xi'] = np.cumsum(local_data_sort_cent.xi) / X
+        local_data_sort_cent['Yi'] = np.cumsum(local_data_sort_cent.yi) / Y
+
+
+        local_RCE = (shift(local_data_sort_cent.Xi, 1, cval=np.NaN) * local_data_sort_cent.Yi).sum() - \
+                    (local_data_sort_cent.Xi * shift(local_data_sort_cent.Yi, 1, cval=np.NaN)).sum()
+        
+        local_RCEs[i] = local_RCE
+        
+    return local_RCEs
     
