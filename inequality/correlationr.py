65a3b68d715470b2f52361c4b810f71de6256be0
renanxcortes <renanxcortes@gmail.com>
2018-10-11 13:15:14 -0700

    adding correlatio ratio and removing  CISO, RI and SM

---
+++ inequality/correlationr.py
@@ -0,0 +1,117 @@
+"""
+Correlation Segregation Metrics
+"""
+
+__author__ = "Renan X. Cortes <renanc@ucr.edu> and Sergio J. Rey <sergio.rey@ucr.edu>"
+
+import numpy as np
+import pandas as pd
+
+__all__ = ['CorrelationR']
+
+
+def _correlationr(data, group_pop_var, total_pop_var):
+    """
+    Calculation of Correlation Ratio index
+
+    Parameters
+    ----------
+
+    data          : a pandas DataFrame
+    
+    group_pop_var : string
+                    The name of variable in data that contains the population size of the group of interest (X)
+                    
+    total_pop_var : string
+                    The name of variable in data that contains the total population of the unit
+
+    Attributes
+    ----------
+
+    v : float
+        Correlation Ratio Index
+
+    Notes
+    -----
+    
+    Based on Massey, Douglas S., and Nancy A. Denton. "The dimensions of residential segregation." Social forces 67.2 (1988): 281-315.
+
+    """
+    if((type(group_pop_var) is not str) or (type(total_pop_var) is not str)):
+        raise TypeError('group_pop_var and total_pop_var must be strings')
+    
+    if ((group_pop_var not in data.columns) or (total_pop_var not in data.columns)):    
+        raise ValueError('group_pop_var and total_pop_var must be variables of data')
+
+    data = data.rename(columns={group_pop_var: 'group_pop_var', 
+                                total_pop_var: 'total_pop_var'})
+    
+    if any(data.total_pop_var < data.group_pop_var):    
+        raise ValueError('Group of interest population must equal or lower than the total population of the units.')
+   
+    data = data.assign(xi = data.group_pop_var,
+                       ti = data.total_pop_var)
+    X = data.xi.sum()
+    T = data.ti.sum()
+    P = X / T
+    
+    xPx = ((data.xi / X) * (data.xi / data.ti)).sum()
+
+    V = (xPx - P) / (1 - P)
+    
+    return V
+
+
+class CorrelationR:
+    """
+    Classic Correlation Ratio Index
+
+    Parameters
+    ----------
+
+    data          : a pandas DataFrame
+    
+    group_pop_var : string
+                    The name of variable in data that contains the population size of the group of interest (X)
+                    
+    total_pop_var : string
+                    The name of variable in data that contains the total population of the unit
+
+    Attributes
+    ----------
+
+    v : float
+        Correlation Ratio Index
+        
+    Examples
+    --------
+    In this example, we will calculate the Correlation Ratio Index (V) for the Riverside County using the census tract data of 2010.
+    The group of interest is non-hispanic black people which is the variable nhblk10 in the dataset.
+    
+    Firstly, we need to read the data:
+    
+    >>> url = 'https://raw.githubusercontent.com/renanxcortes/inequality-segregation-supplementary-files/master/data/std_2010_fullcount.csv'
+    >>> census_2010 = pd.read_csv(url, encoding = "ISO-8859-1", sep = ",")
+    
+    Then, we filter only for the desired county (in this case, Riverside County):
+    
+    >>> df = census_2010.loc[census_2010.county == "Riverside County"][['pop10','nhblk10']]
+    
+    The value is estimated below.
+    
+    >>> correlationr_index = CorrelationR(df, 'nhblk10', 'pop10')
+    >>> correlationr_index.v
+    0.048716810856363923
+    
+    Notes
+    -----
+    
+    Based on Massey, Douglas S., and Nancy A. Denton. "The dimensions of residential segregation." Social forces 67.2 (1988): 281-315.
+
+    """
+
+    def __init__(self, data, group_pop_var, total_pop_var):
+
+        self.v = _correlationr(data, group_pop_var, total_pop_var)
+
+
