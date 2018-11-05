18d2def2d66909be3f1c3b56045ea2762f078040
renanxcortes <renanxcortes@gmail.com>
2018-11-05 15:09:35 -0800

    add a wrapper to perform inference on a single segregation measure

---
+++ inequality/infer_segregation.py
@@ -0,0 +1,234 @@
+"""
+Inference Wrappers for Segregation measures
+"""
+
+__author__ = "Renan X. Cortes <renanc@ucr.edu> and Sergio J. Rey <sergio.rey@ucr.edu>"
+
+
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+
+__all__ = ['Infer_Segregation']
+
+"""
+Inference Wrappers for Segregation measures
+"""
+
+__author__ = "Renan X. Cortes <renanc@ucr.edu> and Sergio J. Rey <sergio.rey@ucr.edu>"
+
+
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+
+__all__ = ['Infer_Segregation']
+
+def _infer_segregation(seg_class, iterations = 500, null_approach = "systematic", **kwargs):
+    '''
+    Perform inference for a single segregation measure
+
+    Parameters
+    ----------
+
+    seg_class: a PySAL segregation object
+    
+    iterations: number of iterations under null hyphothesis
+    
+    null_approach: argument that specifies which type of null hyphotesis the inference will iterate.
+    
+        "systematic"             : assumes that every group has the same probability with restricted conditional probabilities p_0_j = p_1_j = p_j = n_j/n (multinomial distribution).
+        "eveness"                : assumes that each spatial unit has the same global probability of drawing elements from the minority group of the fixed total unit population (binomial distribution).
+        
+        "permutation"            : randomly allocates the units over space keeping the original values.
+        
+        "systematic_permutation" : assumes absence os systematic segregation and randomly allocates the units over space.
+        "even_permutation"       : assumes the same global probability of drawning elements from the minority group in each spatial unit and randomly allocates the units over space.
+    
+    **kwargs: customizable parameters to pass to the segregation measures. Usually they need to be the same input that the seg_class was built.
+    
+    Attributes
+    ----------
+
+    p_value     : float
+                  One-Tailed p-value
+    
+    est_sim     : numpy array
+                  Estimates of the segregation measure under the null hypothesis
+                
+    Notes
+    -----
+    The one-tailed p_value attribute might not be appropriate for some measures. Therefore, it is better to rely on the est_sim attribute.
+    
+    '''
+    
+    point_estimation = seg_class.statistic
+    data             = seg_class.core_data
+    
+    if (null_approach == "systematic"):
+    
+        data['other_group_pop'] = data['total_pop_var'] - data['group_pop_var']
+        p_j = data['total_pop_var'] / data['total_pop_var'].sum()
+
+        # Group 0: minority group
+        p0_i = p_j
+        n0 = data['group_pop_var'].sum()
+        sim0 = np.random.multinomial(n0, p0_i, size = iterations)
+
+        # Group 1: complement group
+        p1_i = p_j
+        n1 = data['other_group_pop'].sum()
+        sim1 = np.random.multinomial(n1, p1_i, size = iterations)
+
+        Estimates_Stars = np.empty(iterations)
+        
+        for i in np.array(range(iterations)):
+            data_aux = {'simul_group': sim0[i].tolist(), 'simul_tot': (sim0[i] + sim1[i]).tolist()}
+            df_aux = pd.DataFrame.from_dict(data_aux)
+            df_aux = gpd.GeoDataFrame(df_aux)
+            df_aux['geometry'] = data['geometry']
+            Estimates_Stars[i] = seg_class._function(df_aux, 'simul_group', 'simul_tot', **kwargs)[0]
+
+        # One-Tailed p-value
+        p_value = sum(Estimates_Stars > point_estimation) / iterations
+    
+    
+    if (null_approach == "eveness"):
+        
+        p_null = data['group_pop_var'].sum() / data['total_pop_var'].sum()
+        
+        Estimates_Stars = np.empty(iterations)
+        
+        for i in np.array(range(iterations)):
+            sim = np.random.binomial(n = np.array([data['total_pop_var'].tolist()]), 
+                                     p = p_null)
+            data_aux = {'simul_group': sim[0], 'simul_tot': data['total_pop_var'].tolist()}
+            df_aux = pd.DataFrame.from_dict(data_aux)
+            df_aux = gpd.GeoDataFrame(df_aux)
+            df_aux['geometry'] = data['geometry']
+            Estimates_Stars[i] = seg_class._function(df_aux, 'simul_group', 'simul_tot', **kwargs)[0]
+            
+        # One-Tailed p-value
+        p_value = sum(Estimates_Stars > point_estimation) / iterations
+        
+        
+    if (null_approach == "permutation"):
+        
+        if (str(type(data)) != '<class \'geopandas.geodataframe.GeoDataFrame\'>'):
+            raise TypeError('data is not a GeoDataFrame, therefore, this null approach does not apply.')
+        
+        Estimates_Stars = np.empty(iterations)
+        
+        for i in np.array(range(iterations)):
+            data = data.assign(geometry = data['geometry'][list(np.random.choice(data.shape[0], data.shape[0], replace = False))].reset_index()['geometry'])
+            df_aux = data
+            Estimates_Stars[i] = seg_class._function(df_aux, 'group_pop_var', 'total_pop_var', **kwargs)[0]
+            
+        # One-Tailed p-value
+        p_value = sum(Estimates_Stars > point_estimation) / iterations
+
+    
+    
+    if (null_approach == "systematic_permutation"):
+        
+        if (str(type(data)) != '<class \'geopandas.geodataframe.GeoDataFrame\'>'):
+            raise TypeError('data is not a GeoDataFrame, therefore, this null approach does not apply.')
+    
+        data['other_group_pop'] = data['total_pop_var'] - data['group_pop_var']
+        p_j = data['total_pop_var'] / data['total_pop_var'].sum()
+
+        # Group 0: minority group
+        p0_i = p_j
+        n0 = data['group_pop_var'].sum()
+        sim0 = np.random.multinomial(n0, p0_i, size = iterations)
+
+        # Group 1: complement group
+        p1_i = p_j
+        n1 = data['other_group_pop'].sum()
+        sim1 = np.random.multinomial(n1, p1_i, size = iterations)
+
+        Estimates_Stars = np.empty(iterations)
+        
+        for i in np.array(range(iterations)):
+            data_aux = {'simul_group': sim0[i].tolist(), 'simul_tot': (sim0[i] + sim1[i]).tolist()}
+            df_aux = pd.DataFrame.from_dict(data_aux)
+            df_aux = gpd.GeoDataFrame(df_aux)
+            df_aux['geometry'] = data['geometry']
+            df_aux = df_aux.assign(geometry = df_aux['geometry'][list(np.random.choice(df_aux.shape[0], df_aux.shape[0], replace = False))].reset_index()['geometry'])
+            Estimates_Stars[i] = seg_class._function(df_aux, 'simul_group', 'simul_tot', **kwargs)[0]
+
+        # One-Tailed p-value
+        p_value = sum(Estimates_Stars > point_estimation) / iterations
+    
+    
+    
+    if (null_approach == "even_permutation"):
+        
+        if (str(type(data)) != '<class \'geopandas.geodataframe.GeoDataFrame\'>'):
+            raise TypeError('data is not a GeoDataFrame, therefore, this null approach does not apply.')
+        
+        p_null = data['group_pop_var'].sum() / data['total_pop_var'].sum()
+        
+        Estimates_Stars = np.empty(iterations)
+        
+        for i in np.array(range(iterations)):
+            sim = np.random.binomial(n = np.array([data['total_pop_var'].tolist()]), 
+                                     p = p_null)
+            data_aux = {'simul_group': sim[0], 'simul_tot': data['total_pop_var'].tolist()}
+            df_aux = pd.DataFrame.from_dict(data_aux)
+            df_aux = gpd.GeoDataFrame(df_aux)
+            df_aux['geometry'] = data['geometry']
+            df_aux = df_aux.assign(geometry = df_aux['geometry'][list(np.random.choice(df_aux.shape[0], df_aux.shape[0], replace = False))].reset_index()['geometry'])
+            Estimates_Stars[i] = seg_class._function(df_aux, 'simul_group', 'simul_tot', **kwargs)[0]
+            
+        # One-Tailed p-value
+        p_value = sum(Estimates_Stars > point_estimation) / iterations
+    
+    
+        
+    return p_value, Estimates_Stars
+
+
+
+class Infer_Segregation:
+    '''
+    Perform inference for a single segregation measure
+
+    Parameters
+    ----------
+
+    seg_class: a PySAL segregation object
+    
+    iterations: number of iterations under null hyphothesis
+    
+    null_approach: argument that specifies which type of null hyphotesis the inference will iterate.
+    
+        "systematic"             : assumes that every group has the same probability with restricted conditional probabilities p_0_j = p_1_j = p_j = n_j/n (multinomial distribution).
+        "eveness"                : assumes that each spatial unit has the same global probability of drawing elements from the minority group of the fixed total unit population (binomial distribution).
+        
+        "permutation"            : randomly allocates the units over space keeping the original values.
+        
+        "systematic_permutation" : assumes absence os systematic segregation and randomly allocates the units over space.
+        "even_permutation"       : assumes the same global probability of drawning elements from the minority group in each spatial unit and randomly allocates the units over space.
+    
+    **kwargs: customizable parameters to pass to the segregation measures. Usually they need to be the same input that the seg_class was built.
+    
+    Attributes
+    ----------
+
+    p_value     : float
+                  One-Tailed p-value
+    
+    est_sim     : numpy array
+                  Estimates of the segregation measure under the null hypothesis
+                
+    Notes
+    -----
+    The one-tailed p_value attribute might not be appropriate for some measures. Therefore, it is better to rely on the est_sim attribute.
+    
+    '''
+
+    def __init__(self, seg_class, iterations = 500, null_approach = "systematic", **kwargs):
+
+        self.p_value = _infer_segregation(seg_class, iterations, null_approach, **kwargs)[0]
+        self.est_sim = _infer_segregation(seg_class, iterations, null_approach, **kwargs)[1]
