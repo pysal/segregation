7156a6c6a40ff61d5399d6c50df55d9452d36804
renanxcortes <renanxcortes@gmail.com>
2018-11-07 11:15:24 -0800

    adds a two_tailed argument and a statistic attribute

--- inequality/infer_segregation.py
+++ inequality/infer_segregation.py
@@ -24,18 +24,18 @@ import geopandas as gpd
 
 __all__ = ['Infer_Segregation']
 
-def _infer_segregation(seg_class, iterations = 500, null_approach = "systematic", **kwargs):
+def _infer_segregation(seg_class, iterations = 500, null_approach = "systematic", two_tailed = True, **kwargs):
     '''
     Perform inference for a single segregation measure
 
     Parameters
     ----------
 
-    seg_class: a PySAL segregation object
+    seg_class     : a PySAL segregation object
     
-    iterations: number of iterations under null hyphothesis
+    iterations    : number of iterations under null hyphothesis
     
-    null_approach: argument that specifies which type of null hyphotesis the inference will iterate.
+    null_approach : argument that specifies which type of null hyphotesis the inference will iterate.
     
         "systematic"             : assumes that every group has the same probability with restricted conditional probabilities p_0_j = p_1_j = p_j = n_j/n (multinomial distribution).
         "eveness"                : assumes that each spatial unit has the same global probability of drawing elements from the minority group of the fixed total unit population (binomial distribution).
@@ -45,22 +45,31 @@ def _infer_segregation(seg_class, iterations = 500, null_approach = "systematic"
         "systematic_permutation" : assumes absence os systematic segregation and randomly allocates the units over space.
         "even_permutation"       : assumes the same global probability of drawning elements from the minority group in each spatial unit and randomly allocates the units over space.
     
+    two_tailed    : boolean
+                    If True, p_value is two-tailed. Otherwise, it is right one-tailed.
+    
     **kwargs: customizable parameters to pass to the segregation measures. Usually they need to be the same input that the seg_class was built.
     
     Attributes
     ----------
 
     p_value     : float
-                  One-Tailed p-value
+                  Pseudo One or Two-Tailed p-value estimated from the simulations
     
     est_sim     : numpy array
                   Estimates of the segregation measure under the null hypothesis
+                  
+    statistic   : float
+                  The point estimation of the segregation measure that is under test
                 
     Notes
     -----
-    The one-tailed p_value attribute might not be appropriate for some measures. Therefore, it is better to rely on the est_sim attribute.
+    The one-tailed p_value attribute might not be appropriate for some measures, as the two-tailed. Therefore, it is better to rely on the est_sim attribute.
     
     '''
+    
+    if (type(two_tailed) is not bool):
+        raise TypeError('two_tailed is not a boolean object')
     
     point_estimation = seg_class.statistic
     data             = seg_class.core_data
@@ -93,9 +102,6 @@ def _infer_segregation(seg_class, iterations = 500, null_approach = "systematic"
             df_aux = gpd.GeoDataFrame(df_aux)
             df_aux['geometry'] = data['geometry']
             Estimates_Stars[i] = seg_class._function(df_aux, 'simul_group', 'simul_tot', **kwargs)[0]
-
-        # One-Tailed p-value
-        p_value = sum(Estimates_Stars > point_estimation) / iterations
     
     
     if (null_approach == "eveness"):
@@ -115,9 +121,6 @@ def _infer_segregation(seg_class, iterations = 500, null_approach = "systematic"
                 df_aux['geometry'] = data['geometry']
             
             Estimates_Stars[i] = seg_class._function(df_aux, 'simul_group', 'simul_tot', **kwargs)[0]
-            
-        # One-Tailed p-value
-        p_value = sum(Estimates_Stars > point_estimation) / iterations
         
         
     if (null_approach == "permutation"):
@@ -131,11 +134,7 @@ def _infer_segregation(seg_class, iterations = 500, null_approach = "systematic"
             data = data.assign(geometry = data['geometry'][list(np.random.choice(data.shape[0], data.shape[0], replace = False))].reset_index()['geometry'])
             df_aux = data
             Estimates_Stars[i] = seg_class._function(df_aux, 'group_pop_var', 'total_pop_var', **kwargs)[0]
-            
-        # One-Tailed p-value
-        p_value = sum(Estimates_Stars > point_estimation) / iterations
 
-    
     
     if (null_approach == "systematic_permutation"):
         
@@ -163,11 +162,7 @@ def _infer_segregation(seg_class, iterations = 500, null_approach = "systematic"
             df_aux = gpd.GeoDataFrame(df_aux)
             df_aux['geometry'] = data['geometry']
             df_aux = df_aux.assign(geometry = df_aux['geometry'][list(np.random.choice(df_aux.shape[0], df_aux.shape[0], replace = False))].reset_index()['geometry'])
-            Estimates_Stars[i] = seg_class._function(df_aux, 'simul_group', 'simul_tot', **kwargs)[0]
-
-        # One-Tailed p-value
-        p_value = sum(Estimates_Stars > point_estimation) / iterations
-    
+            Estimates_Stars[i] = seg_class._function(df_aux, 'simul_group', 'simul_tot', **kwargs)[0]  
     
     
     if (null_approach == "even_permutation"):
@@ -188,13 +183,14 @@ def _infer_segregation(seg_class, iterations = 500, null_approach = "systematic"
             df_aux['geometry'] = data['geometry']
             df_aux = df_aux.assign(geometry = df_aux['geometry'][list(np.random.choice(df_aux.shape[0], df_aux.shape[0], replace = False))].reset_index()['geometry'])
             Estimates_Stars[i] = seg_class._function(df_aux, 'simul_group', 'simul_tot', **kwargs)[0]
-            
-        # One-Tailed p-value
+
+    
+    if not two_tailed:
         p_value = sum(Estimates_Stars > point_estimation) / iterations
-    
-    
+    else:
+        p_value = (sum(Estimates_Stars > abs(point_estimation)) + sum(Estimates_Stars < -abs(point_estimation))) / iterations
         
-    return p_value, Estimates_Stars
+    return p_value, Estimates_Stars, point_estimation
 
 
 
@@ -205,11 +201,11 @@ class Infer_Segregation:
     Parameters
     ----------
 
-    seg_class: a PySAL segregation object
+    seg_class     : a PySAL segregation object
     
-    iterations: number of iterations under null hyphothesis
+    iterations    : number of iterations under null hyphothesis
     
-    null_approach: argument that specifies which type of null hyphotesis the inference will iterate.
+    null_approach : argument that specifies which type of null hyphotesis the inference will iterate.
     
         "systematic"             : assumes that every group has the same probability with restricted conditional probabilities p_0_j = p_1_j = p_j = n_j/n (multinomial distribution).
         "eveness"                : assumes that each spatial unit has the same global probability of drawing elements from the minority group of the fixed total unit population (binomial distribution).
@@ -219,26 +215,33 @@ class Infer_Segregation:
         "systematic_permutation" : assumes absence os systematic segregation and randomly allocates the units over space.
         "even_permutation"       : assumes the same global probability of drawning elements from the minority group in each spatial unit and randomly allocates the units over space.
     
+    two_tailed    : boolean
+                    If True, p_value is two-tailed. Otherwise, it is right one-tailed.
+    
     **kwargs: customizable parameters to pass to the segregation measures. Usually they need to be the same input that the seg_class was built.
     
     Attributes
     ----------
 
     p_value     : float
-                  One-Tailed p-value
+                  Pseudo One or Two-Tailed p-value estimated from the simulations
     
     est_sim     : numpy array
                   Estimates of the segregation measure under the null hypothesis
+                  
+    statistic   : float
+                  The point estimation of the segregation measure that is under test
                 
     Notes
     -----
-    The one-tailed p_value attribute might not be appropriate for some measures. Therefore, it is better to rely on the est_sim attribute.
+    The one-tailed p_value attribute might not be appropriate for some measures, as the two-tailed. Therefore, it is better to rely on the est_sim attribute.
     
     '''
 
-    def __init__(self, seg_class, iterations = 500, null_approach = "systematic", **kwargs):
+    def __init__(self, seg_class, iterations = 500, null_approach = "systematic", two_tailed = True, **kwargs):
         
-        aux = _infer_segregation(seg_class, iterations, null_approach, **kwargs)
+        aux = _infer_segregation(seg_class, iterations, null_approach, two_tailed, **kwargs)
 
-        self.p_value = aux[0]
-        self.est_sim = aux[1]
+        self.p_value   = aux[0]
+        self.est_sim   = aux[1]
+        self.statistic = aux[2]
