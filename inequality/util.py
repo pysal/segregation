2471090b551f476558b54f8893407b5d2b3e2659
renanxcortes <renanxcortes@gmail.com>
2018-11-11 07:10:20 -0800

    adding notebook for paper application

--- inequality/util.py
+++ inequality/util.py
@@ -8,6 +8,9 @@ import numpy as np
 import pandas as pd
 import libpysal
 import geopandas as gpd
+from warnings import warn
+from libpysal.weights import Queen, KNN
+from libpysal.weights.util import attach_islands
 
 
 def _return_length_weighted_w(data):
@@ -30,6 +33,14 @@ def _return_length_weighted_w(data):
     
     data['index'] = data.index
     w = libpysal.weights.Queen.from_dataframe(data, ids = data.index.tolist())
+    
+    if not len(w.islands):
+        w = w
+    else:
+        warn('There are some islands in the GeoDataFrame. They are going to be attached to its closest neighbors to calculate shared border.')
+        w_knn1 = libpysal.weights.KNN.from_dataframe(data, ids = data.index.tolist(), k = 1)
+        w = attach_islands(w, w_knn1)
+    
     
     adjlist = w.to_adjlist().merge(data[['index', 'geometry']], left_on='focal', right_on='index', how='left')\
               .drop('index', axis=1)\
