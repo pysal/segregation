a57a44473dff9f0abab426408b54c72bf5a1bc94
renanxcortes <renanxcortes@gmail.com>
2018-11-16 14:55:47 -0800

    include test for dissimilarity function

---
+++ inequality/tests/test_dissim.py
@@ -0,0 +1,17 @@
+import unittest
+import libpysal
+import geopandas as gpd
+import numpy as np
+from inequality.dissimilarity import Dissim
+
+
+class Dissim_Tester(unittest.TestCase):
+
+    def test_Dissim(self):
+		s_map = gpd.read_file(libpysal.examples.get_path("sacramentot2.shp"))
+		df = s_map[['geometry', 'HISP_', 'TOT_POP']]
+		D = Dissim(df, 'HISP_', 'TOT_POP')
+		np.testing.assert_almost_equal(D.statistic, 0.32184656076566864)
+
+if __name__ == '__main__':
+    unittest.main()
