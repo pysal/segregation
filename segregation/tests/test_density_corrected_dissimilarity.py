ba820d5fb4a3eabf35c8e40abbaa9bd3991e16c1
renanxcortes <renanxcortes@gmail.com>
2018-12-05 18:15:34 -0800

    Update test_density_corrected_dissimilarity.py

--- segregation/tests/test_density_corrected_dissimilarity.py
+++ segregation/tests/test_density_corrected_dissimilarity.py
@@ -11,7 +11,7 @@ class Density_Corrected_Dissim_Tester(unittest.TestCase):
         s_map = gpd.read_file(libpysal.examples.get_path("sacramentot2.shp"))
         df = s_map[['geometry', 'HISP_', 'TOT_POP']]
         index = Density_Corrected_Dissim(df, 'HISP_', 'TOT_POP')
-        np.testing.assert_almost_equal(index.statistic, 0.2962244660362303)
+        np.testing.assert_almost_equal(index.statistic, 0.2952051469981647)
 
 if __name__ == '__main__':
     unittest.main()
