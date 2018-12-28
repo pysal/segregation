f35e488f1e39761fbd770abbde6c9294459a6091
renanxcortes <renanxcortes@gmail.com>
2018-12-28 11:46:19 -0800

    changing to 1.0.0 version in the setup

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ import os
 with open('README.md', 'r', encoding='utf8') as file:
     long_description = file.read()
 
-MAJOR = 0
-MINOR = 1
+MAJOR = 1
+MINOR = 0
 MICRO = 0
 ISRELEASED = False
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
