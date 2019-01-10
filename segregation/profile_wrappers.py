b38bdf0224116a15d12495d3cd9de7883eaff6f0
renanxcortes <renanxcortes@gmail.com>
2019-01-09 17:53:51 -0800

    add all indexes to profile wrappers and remove **kwargs

--- segregation/profile_wrappers.py
+++ segregation/profile_wrappers.py
@@ -34,7 +34,7 @@ from segregation.spatial_proximity import Spatial_Proximity
 
 __all__ = ['Profile_Non_Spatial_Segregation', 'Profile_Spatial_Segregation', 'Profile_Segregation']
 
-def _profile_non_spatial_segregation(data, group_pop_var, total_pop_var, **kwargs):
+def _profile_non_spatial_segregation(data, group_pop_var, total_pop_var):
     '''
     Perform point estimation of selected non spatial segregation measures at once
 
@@ -49,8 +49,6 @@ def _profile_non_spatial_segregation(data, group_pop_var, total_pop_var, **kwarg
     total_pop_var : string
                     The name of variable in data that contains the total population of the unit
     
-    **kwargs      : customizable parameters to pass to the segregation measures
-    
     Attributes
     ----------
 
@@ -59,18 +57,18 @@ def _profile_non_spatial_segregation(data, group_pop_var, total_pop_var, **kwarg
     
     '''
     
-    D = Dissim(data, group_pop_var, total_pop_var, **kwargs)
-    G = Gini_Seg(data, group_pop_var, total_pop_var, **kwargs)
-    H = Entropy(data, group_pop_var, total_pop_var, **kwargs)
-    A = Atkinson(data, group_pop_var, total_pop_var, **kwargs)
-    xPy = Exposure(data, group_pop_var, total_pop_var, **kwargs)
-    xPx = Isolation(data, group_pop_var, total_pop_var, **kwargs)
-    R = Con_Prof(data, group_pop_var, total_pop_var, **kwargs)
-    #Dbc = Bias_Corrected_Dissim(data, group_pop_var, total_pop_var, **kwargs)
-    #Ddc = Density_Corrected_Dissim(data, group_pop_var, total_pop_var, **kwargs)
-    V = Correlation_R(data, group_pop_var, total_pop_var, **kwargs)
-    #Dct = Modified_Dissim(data, group_pop_var, total_pop_var, **kwargs)
-    #Gct = Modified_Gini_Seg(data, group_pop_var, total_pop_var, **kwargs)
+    D = Dissim(data, group_pop_var, total_pop_var)
+    G = Gini_Seg(data, group_pop_var, total_pop_var)
+    H = Entropy(data, group_pop_var, total_pop_var)
+    A = Atkinson(data, group_pop_var, total_pop_var)
+    xPy = Exposure(data, group_pop_var, total_pop_var)
+    xPx = Isolation(data, group_pop_var, total_pop_var)
+    R = Con_Prof(data, group_pop_var, total_pop_var)
+    Dbc = Bias_Corrected_Dissim(data, group_pop_var, total_pop_var)
+    Ddc = Density_Corrected_Dissim(data, group_pop_var, total_pop_var)
+    V = Correlation_R(data, group_pop_var, total_pop_var)
+    Dct = Modified_Dissim(data, group_pop_var, total_pop_var)
+    Gct = Modified_Gini_Seg(data, group_pop_var, total_pop_var)
     
     dictionary = {'Dissimilarity': D.statistic,
                   'Gini': G.statistic,
@@ -79,11 +77,11 @@ def _profile_non_spatial_segregation(data, group_pop_var, total_pop_var, **kwarg
                   'Exposure': xPy.statistic,
                   'Isolation': xPx.statistic,
                   'Concentration Profile': R.statistic,
-                  #'Bias Corrected Dissimilarity': Dbc.statistic,
-                  #'Density Corrected Dissimilarity': Ddc.statistic,
+                  'Bias Corrected Dissimilarity': Dbc.statistic,
+                  'Density Corrected Dissimilarity': Ddc.statistic,
                   'Correlation Ratio': V.statistic,
-                  #'Modified Dissimilarity': Dct.statistic,
-                  #'Modified Gini': Gct.statistic
+                  'Modified Dissimilarity': Dct.statistic,
+                  'Modified Gini': Gct.statistic
                   }
     
     return dictionary
@@ -105,8 +103,6 @@ class Profile_Non_Spatial_Segregation:
     total_pop_var : string
                     The name of variable in data that contains the total population of the unit
     
-    **kwargs      : customizable parameters to pass to the segregation measures
-    
     Attributes
     ----------
 
@@ -115,9 +111,9 @@ class Profile_Non_Spatial_Segregation:
     
     '''
 
-    def __init__(self, data, group_pop_var, total_pop_var, **kwargs):
+    def __init__(self, data, group_pop_var, total_pop_var):
         
-        aux = _profile_non_spatial_segregation(data, group_pop_var, total_pop_var, **kwargs)
+        aux = _profile_non_spatial_segregation(data, group_pop_var, total_pop_var)
 
         self.profile = aux
         
@@ -131,7 +127,7 @@ class Profile_Non_Spatial_Segregation:
 
 
 
-def _profile_spatial_segregation(data, group_pop_var, total_pop_var, **kwargs):
+def _profile_spatial_segregation(data, group_pop_var, total_pop_var):
     '''
     Perform point estimation of selected spatial segregation measures at once
 
@@ -146,8 +142,6 @@ def _profile_spatial_segregation(data, group_pop_var, total_pop_var, **kwargs):
     total_pop_var : string
                     The name of variable in data that contains the total population of the unit
     
-    **kwargs      : customizable parameters to pass to the segregation measures
-    
     Attributes
     ----------
 
@@ -156,24 +150,24 @@ def _profile_spatial_segregation(data, group_pop_var, total_pop_var, **kwargs):
     
     '''
     
-    SD = Spatial_Dissim(data, group_pop_var, total_pop_var, **kwargs)
-    #PARD = Perimeter_Area_Ratio_Spatial_Dissim(data, group_pop_var, total_pop_var, **kwargs)
-    #BSD = Boundary_Spatial_Dissim(data, group_pop_var, total_pop_var, **kwargs)
-    #ACE = Absolute_Centralization(data, group_pop_var, total_pop_var, **kwargs)
-    #ACO = Absolute_Concentration(data, group_pop_var, total_pop_var, **kwargs)
-    #DEL = Delta(data, group_pop_var, total_pop_var, **kwargs)
-    RCE = Relative_Centralization(data, group_pop_var, total_pop_var, **kwargs)
-    RCL = Relative_Clustering(data, group_pop_var, total_pop_var, **kwargs)
-    RCO = Relative_Concentration(data, group_pop_var, total_pop_var, **kwargs)
-    SxPy = Spatial_Exposure(data, group_pop_var, total_pop_var, **kwargs)
-    SxPx = Spatial_Isolation(data, group_pop_var, total_pop_var, **kwargs)
-    SPP = Spatial_Prox_Prof(data, group_pop_var, total_pop_var, **kwargs)
-    SP = Spatial_Proximity(data, group_pop_var, total_pop_var, **kwargs)
+    SD = Spatial_Dissim(data, group_pop_var, total_pop_var)
+    PARD = Perimeter_Area_Ratio_Spatial_Dissim(data, group_pop_var, total_pop_var)
+    BSD = Boundary_Spatial_Dissim(data, group_pop_var, total_pop_var)
+    ACE = Absolute_Centralization(data, group_pop_var, total_pop_var)
+    ACO = Absolute_Concentration(data, group_pop_var, total_pop_var)
+    DEL = Delta(data, group_pop_var, total_pop_var)
+    RCE = Relative_Centralization(data, group_pop_var, total_pop_var)
+    RCL = Relative_Clustering(data, group_pop_var, total_pop_var)
+    RCO = Relative_Concentration(data, group_pop_var, total_pop_var)
+    SxPy = Spatial_Exposure(data, group_pop_var, total_pop_var)
+    SxPx = Spatial_Isolation(data, group_pop_var, total_pop_var)
+    SPP = Spatial_Prox_Prof(data, group_pop_var, total_pop_var)
+    SP = Spatial_Proximity(data, group_pop_var, total_pop_var)
     
     dictionary = {'Spatial Dissimilarity': SD.statistic,
-                  #'Absolute Centralization': ACE.statistic,
-                  #'Absolute Concentration': ACO.statistic,
-                  #'Delta': DEL.statistic,
+                  'Absolute Centralization': ACE.statistic,
+                  'Absolute Concentration': ACO.statistic,
+                  'Delta': DEL.statistic,
                   'Relative Centralization': RCE.statistic,
                   'Relative Clustering': RCL.statistic,
                   'Relative Concentration': RCO.statistic,
@@ -181,8 +175,8 @@ def _profile_spatial_segregation(data, group_pop_var, total_pop_var, **kwargs):
                   'Spatial Isolation': SxPx.statistic,
                   'Spatial Proximity Profile': SPP.statistic,
                   'Spatial Proximity': SP.statistic,
-                  #'Boundary Spatial Dissimilarity': BSD.statistic,
-                  #'Perimeter Area Ratio Spatial Dissimilarity': PARD.statistic
+                  'Boundary Spatial Dissimilarity': BSD.statistic,
+                  'Perimeter Area Ratio Spatial Dissimilarity': PARD.statistic
                   }
     
     return dictionary
@@ -204,8 +198,6 @@ class Profile_Spatial_Segregation:
     total_pop_var : string
                     The name of variable in data that contains the total population of the unit
     
-    **kwargs      : customizable parameters to pass to the segregation measures
-    
     Attributes
     ----------
 
@@ -214,9 +206,9 @@ class Profile_Spatial_Segregation:
     
     '''
 
-    def __init__(self, data, group_pop_var, total_pop_var, **kwargs):
+    def __init__(self, data, group_pop_var, total_pop_var):
         
-        aux = _profile_spatial_segregation(data, group_pop_var, total_pop_var, **kwargs)
+        aux = _profile_spatial_segregation(data, group_pop_var, total_pop_var)
 
         self.profile = aux
 
@@ -237,7 +229,7 @@ class Profile_Spatial_Segregation:
 
 
 
-def _profile_segregation(data, group_pop_var, total_pop_var, **kwargs):
+def _profile_segregation(data, group_pop_var, total_pop_var):
     '''
     Perform point estimation of selected segregation measures at once
 
@@ -252,8 +244,6 @@ def _profile_segregation(data, group_pop_var, total_pop_var, **kwargs):
     total_pop_var : string
                     The name of variable in data that contains the total population of the unit
     
-    **kwargs      : customizable parameters to pass to the segregation measures
-    
     Attributes
     ----------
 
@@ -262,8 +252,8 @@ def _profile_segregation(data, group_pop_var, total_pop_var, **kwargs):
     
     '''
     
-    x = Profile_Non_Spatial_Segregation(data, group_pop_var, total_pop_var, **kwargs).profile
-    y = Profile_Spatial_Segregation(data, group_pop_var, total_pop_var, **kwargs).profile
+    x = Profile_Non_Spatial_Segregation(data, group_pop_var, total_pop_var).profile
+    y = Profile_Spatial_Segregation(data, group_pop_var, total_pop_var).profile
     
     x.update(y)
     
@@ -286,8 +276,6 @@ class Profile_Segregation:
     total_pop_var : string
                     The name of variable in data that contains the total population of the unit
     
-    **kwargs      : customizable parameters to pass to the segregation measures
-    
     Attributes
     ----------
 
@@ -296,9 +284,9 @@ class Profile_Segregation:
     
     '''
 
-    def __init__(self, data, group_pop_var, total_pop_var, **kwargs):
+    def __init__(self, data, group_pop_var, total_pop_var):
         
-        aux = _profile_segregation(data, group_pop_var, total_pop_var, **kwargs)
+        aux = _profile_segregation(data, group_pop_var, total_pop_var)
 
         self.profile = aux
         
