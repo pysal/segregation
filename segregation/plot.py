d6ae0b7dba333d5077c3bfe776556ea248f16edb
eli knaap <ek@knaaptime.com>
2019-03-26 21:03:15 -0700

    add working decomposition func and lorenz plotting func

---
+++ segregation/plot.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+
+
+def lorenz(group_share1, group_share2, label1='', label2=''):
+    """Plot Lorenz curves for two series
+    Convenience function for comparing inequality between two series by
+    plotting their lorenz curves on the same graph
+
+    Parameters
+    ----------
+    group_share1 : pd.Series
+        pandas series with 
+    group_share2 : pd.Series
+        Description of parameter `group_share2`.
+
+    Returns
+    -------
+    type
+        matplotlib Figure.
+
+    """
+    plt.step(group_share1.sort_values(), group_share1.rank(pct=True).sort_values(), label=label1)
+    plt.step(group_share2.sort_values(), group_share2.rank(pct=True).sort_values(), label=label2)
+    if (label1 != '' or label2 != ''):
+        plt.legend()
+    plt.show()
+    fig = plt.gcf()
+    return fig
