fceaa624c37c1a977a70f7341569870c00641538
renanxcortes <renanxcortes@gmail.com>
2019-06-02 19:51:05 -0700

    update test for new formula

--- segregation/tests/test_perimeter_area_ratio_spatial_dissimilarity.py
+++ segregation/tests/test_perimeter_area_ratio_spatial_dissimilarity.py
@@ -11,7 +11,7 @@ class Perimeter_Area_Ratio_Spatial_Dissim_Tester(unittest.TestCase):
         s_map = gpd.read_file(libpysal.examples.get_path("sacramentot2.shp"))
         df = s_map[['geometry', 'HISP_', 'TOT_POP']]
         index = Perimeter_Area_Ratio_Spatial_Dissim(df, 'HISP_', 'TOT_POP')
-        np.testing.assert_almost_equal(index.statistic, 0.3165091834802075)
+        np.testing.assert_almost_equal(index.statistic, 0.3111718061947464)
 
 if __name__ == '__main__':
     unittest.main()
