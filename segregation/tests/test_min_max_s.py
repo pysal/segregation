e6f7ba5013f0106d5abfdf29dfa6c94ac81de909
renanxcortes <renanxcortes@gmail.com>
2019-07-15 13:30:31 -0700

    bug: update class name for testing

--- segregation/tests/test_min_max_s.py
+++ segregation/tests/test_min_max_s.py
@@ -2,14 +2,14 @@ import unittest
 import libpysal
 import geopandas as gpd
 import numpy as np
-from segregation.spatial import MinMaxS
+from segregation.spatial import SpatialMinMax
 
 
-class Min_Max_S_Tester(unittest.TestCase):
-    def test_Min_Max_S(self):
+class SpatialMinMax_Tester(unittest.TestCase):
+    def test_SpatialMinMax(self):
         s_map = gpd.read_file(libpysal.examples.get_path("sacramentot2.shp"))
         df = s_map[['geometry', 'HISP_', 'TOT_POP']]
-        index = MinMaxS(df, 'HISP_', 'TOT_POP')
+        index = SpatialMinMax(df, 'HISP_', 'TOT_POP')
         np.testing.assert_almost_equal(index.statistic, 0.17119951092816454)
 
 
