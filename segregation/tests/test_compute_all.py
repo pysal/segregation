b1a0f4b8dc9a5e5f12079a91bd3342eff0f445bd
renanxcortes <renanxcortes@gmail.com>
2019-07-18 11:00:19 -0700

    bug: change to numpy array for testing

--- segregation/tests/test_compute_all.py
+++ segregation/tests/test_compute_all.py
@@ -23,7 +23,7 @@ class ComputeAll_Tester(unittest.TestCase):
     
         np.random.seed(123)
         res = ComputeAllSegregation(s_map, 'HISP_', 'TOT_POP')
-        np.testing.assert_almost_equal(np.array(res.computed['Value']), array([ 0.32184656,  0.43506511,  0.09459761,  0.15079259,  0.76803845,
+        np.testing.assert_almost_equal(np.array(res.computed['Value']), np.array([ 0.32184656,  0.43506511,  0.09459761,  0.15079259,  0.76803845,
         0.23196155,  0.13768748,  0.32141961,  0.29520516,  0.09164042,
         0.31074587,  0.42179274,  0.26119743,  0.68914224,  0.00518929,
         0.85128245,  0.80449692, -0.11194178,  0.00909563,  0.12733821,
