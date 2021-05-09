ba3fae858c76d7f826ec3593d274909639e74c61
eli knaap <ek@knaaptime.com>
2021-05-09 13:46:50 -0700

    update downloader test

--- segregation/tests/test_network_download.py
+++ segregation/tests/test_network_download.py
@@ -7,4 +7,4 @@ from segregation.util import get_osm_network
 def test_network_download():
     s_map = gpd.read_file(load_example("Sacramento1").get_path("sacramentot2.shp"))
     net = get_osm_network(s_map.iloc[[1]])
-    assert net.edges_df.shape == (7942, 3)
+    assert net.edges_df.shape[0] >= 7942
