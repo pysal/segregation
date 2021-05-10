77af250acc4c66fb48130f49b76603324efaa431
eli knaap <ek@knaaptime.com>
2021-05-10 14:28:57 -0700

    update tests

--- segregation/tests/test_distance_decay_isolation.py
+++ segregation/tests/test_distance_decay_isolation.py
@@ -10,7 +10,7 @@ class Distance_Decay_Isolation_Tester(unittest.TestCase):
         s_map = gpd.read_file(load_example("Sacramento1").get_path("sacramentot2.shp"))
         df = s_map[['geometry', 'HISP', 'TOT_POP']]
         index = DistanceDecayIsolation(df, 'HISP', 'TOT_POP')
-        np.testing.assert_almost_equal(index.statistic, 0.19362385990000566)
+        np.testing.assert_almost_equal(index.statistic, 0.15468010320797765)
 
 
 if __name__ == '__main__':
