ea8374215f4126205da37e5ab4ac430dc32fcbe9
eli knaap <ek@knaaptime.com>
2021-07-03 19:38:03 -0700

    include systematic

--- segregation/inference/randomization.py
+++ segregation/inference/randomization.py
@@ -48,7 +48,34 @@ def simulate_reallocation_slow(df, group=None, total=None, groups=None):
 
 
 def simulate_reallocation(df, group=None, total=None, groups=None):
+    """Simulate the random reallocation of existing population groups across spatial units.
 
+    Parameters
+    ----------
+    df : geopandas.GeoDataFrame
+        geodataframe with population data to be randomized
+    group : str, optional
+        name of column on geodataframe that holds the group total
+        (for use with single group indices)
+    total : str, optional
+        name of column on geodataframe that holds the total population for
+        each unit (for use with single group indices)
+    groups : list, optional
+        list of columns on inut dataframe that hold total population counts
+        for each group of interest
+
+    Returns
+    -------
+    geopandas.GeoDataFrame
+        geodataframe with randomly reallocated population
+
+    Notes
+    -------
+    Simulates the random redistribution of the existing population. Given a pool
+    of the total population in the region, randomly allocate each person to a 
+    geographic unit, subject to the total capacity of each unit. Results are
+    guaranteed to respect regional and local totals for groups and geographic units
+    """
     df = df.copy()
     # ensure we have a coilumn named "index"
     df = df.reset_index(drop=True)
@@ -62,12 +89,14 @@ def simulate_reallocation(df, group=None, total=None, groups=None):
         df[total] = df[total].astype(int)
         df["other"] = df[total] - df[group]
         groups = [group, "other"]
+
     # create a list of group membership for each person
     members = [[group for i in range(df[group].sum().astype(int))] for group in groups]
     pop_groups = list(itertools.chain.from_iterable(members))
 
     # create a  list of 1s representing the population in each unit
     df["people"] = df[total].apply(lambda x: [1 for i in range(x)])
+
     # explode the dataframe to have n_rows = total_population
     df = df["people"].explode().reset_index()["index"].to_frame()
     df["groups"] = pop_groups
@@ -79,18 +108,110 @@ def simulate_reallocation(df, group=None, total=None, groups=None):
     df = df.groupby("index")["groups"].value_counts().unstack()
     df[total] = df[groups].sum(axis=1)
     df = df.join(geoms)
+
     return gpd.GeoDataFrame(df, geometry=geoms.geometry.name)
 
 
 def simulate_evenness(df, group=None, total=None, groups=None):
+    """Simulate even redistribution of population groups across spatial units.
+
+    Parameters
+    ----------
+    df : geopandas.GeoDataFrame
+        geodataframe with population data to be randomized
+    group : str, optional
+        name of column on geodataframe that holds the group total
+        (for use with single group indices)
+    total : str, optional
+        name of column on geodataframe that holds the total population for
+        each unit (for use with single group indices)
+    groups : list, optional
+        list of columns on inut dataframe that hold total population counts
+        for each group of interest
+
+    Returns
+    -------
+    geopandas.GeoDataFrame
+        geodataframe with evenly distributed population groups
+
+    Notes
+    -------
+    Simulates the random allocation of racial groups, given the total population of
+    each geographic unit (randomizes group totals for each location). Given the total
+    population of each location, take draws from a multinomial distribution to assign
+    group categories for each person, where the probability of each group is equal to
+    its regional share. Results are guaranteed to match regional and local population
+    totals, but will include variation in the relative share of each group
+    """
     df = df.copy()
-    df[[group, total]] = df[[group, total]].astype(int)
+    geoms = df[df.geometry.name].values
     if group:
+        df[[group, total]] = df[[group, total]].astype(int)
         p_null = df[group].sum() / df[total].sum()
 
         output = gpd.GeoDataFrame()
         output[group] = np.random.binomial(n=df[total].values, p=p_null)
         output[total] = df[total].tolist()
-        output["geometry"] = df[df.geometry.name]
+    if groups:
+        df = df[groups]
+        global_prob_vector = df.sum(axis=0) / df.sum().sum()
+        t = df[groups].sum(axis=1).astype(int)
 
-    return output
+        simul = list(
+            map(lambda i: list(np.random.multinomial(i, global_prob_vector)), t)
+        )
+        output = pd.DataFrame(simul, columns=groups)
+    output["geometry"] = geoms
+
+    return gpd.GeoDataFrame(output)
+
+
+def simulate_systematic_randomization(df, group=None, total=None, groups=None):
+    """Simulate even redistribution of population groups across spatial units.
+
+    Parameters
+    ----------
+    df : geopandas.GeoDataFrame
+        geodataframe with population data to be randomized
+    group : str, optional
+        name of column on geodataframe that holds the group total
+        (for use with single group indices)
+    total : str, optional
+        name of column on geodataframe that holds the total population for
+        each unit (for use with single group indices)
+    groups : list, optional
+        list of columns on inut dataframe that hold total population counts
+        for each group of interest
+
+    Returns
+    -------
+    geopandas.GeoDataFrame
+        geodataframe with systematically randomized population groups
+
+    Notes
+    -------
+    Simulates the random allocation across geographic units, given the total population
+    of each group (randomizes location totals for each group). Given the total population of
+    each group in the region, take draws from a multinomial distribution where the
+    probability of choosing each geographic unit is equal to the total regional share
+    currently residing in the unit. Results will include regional and local variation in
+    both total population and relative group shares.
+    """
+    if groups:
+        df[total] = df[groups].sum(axis=1)
+    if group:
+        assert (
+            total
+        ), "If simulating a single group, you must also supply a total population column"
+        df["other_group_pop"] = df[total] - df[group]
+        groups = [group, "other_group_pop"]
+
+    p_j = df[total] / df[total].sum()
+    data_aux = {}
+    for group in groups:
+        n = df[group].sum()
+        sim = np.random.multinomial(n, p_j)
+        data_aux[group] = sim.tolist()
+    df_aux = pd.DataFrame.from_dict(data_aux)
+
+    return df_aux
