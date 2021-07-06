c0e073d05933a7617f2cc549ada67252ab97a416
eli knaap <ek@knaaptime.com>
2021-07-06 16:59:02 -0700

    remove slow reallocation function

--- segregation/inference/randomization.py
+++ segregation/inference/randomization.py
@@ -48,45 +48,6 @@ def simulate_null(
     return estimates
 
 
-def simulate_reallocation_slow(df, group=None, total=None, groups=None):
-    df = df.copy()
-    df = df.reset_index(drop=True)
-    # shuffle the spatial units so we allocate at random
-    units = df.index.copy().values
-    np.random.shuffle(units)
-    units = list(units)
-    if group:
-        df[group] = df[group].astype(int)
-        df[total] = df[total].astype(int)
-        df["other"] = df[total] - df[group]
-        # create a vector of "persons"
-        g1_pop = [group for n in range(0, df[group].sum() + 1)]
-        g2_pop = ["other" for n in range(0, df["other"].sum() + 1)]
-        population = pd.DataFrame()
-        population["pop"] = pd.Series(g1_pop + g2_pop)
-        # set of indices we'll use to choose from
-        pop_idx = population.index.to_series()
-
-        with tqdm(total=len(units)) as pbar:
-            while units:
-                unit = units[0]
-                # use the total population of selected unit to draw indices from the population
-                unit_pop = np.random.choice(
-                    pop_idx.values, df.loc[unit, total], replace=False
-                )
-                # assign the unit ID to each chosen person
-                population.loc[unit_pop, "id"] = unit
-
-                # remove indices so they can't be chosen again
-                units.pop(0)
-                pop_idx = pop_idx[~pop_idx.index.isin(unit_pop)]
-                pbar.update(1)
-        output = population.groupby("id")["pop"].value_counts().unstack()
-        output.columns.name = None
-        output = df[[df.geometry.name]].join(output)
-        return output
-
-
 def simulate_reallocation(df, group=None, total=None, groups=None):
     """Simulate the random reallocation of existing population groups across spatial units.
 
