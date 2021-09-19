fa6a08ca41982bcd07081800a46ca83b1f649064
Serge Rey <sjsrey@gmail.com>
2021-09-19 11:01:29 -0700

    Move version def to fix circular import

--- segregation/__init__.py
+++ segregation/__init__.py
@@ -5,9 +5,9 @@
 """
 from . import batch, decomposition, dynamics, inference, local, multigroup, singlegroup, util
 
+from . import _version
+__version__ = _version.get_versions()['version']
 
 # below handles deprecation warnings. Remove in 2.2.0
 from . import  aspatial, spatial
 
-from . import _version
-__version__ = _version.get_versions()['version']
