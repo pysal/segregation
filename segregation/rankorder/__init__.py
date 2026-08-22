6e44ccd035b975d42a48ae3282dcc9ccf4c12327
knaaptime <knaaptime@magneto.local>
2026-08-22 14:50:03 -0700

    rank order indices

---
+++ segregation/rankorder/__init__.py
@@ -0,0 +1,5 @@
+"""Rank-order (ordinal) segregation indices."""
+
+from .rank_order_info_theory import RankOrderInfoTheory
+from .rank_order_sqrt import RankOrderSqrt
+from .rank_order_variance_ratio import RankOrderVarianceRatio
